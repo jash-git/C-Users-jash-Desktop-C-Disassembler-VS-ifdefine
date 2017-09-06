74184fd876d09a76bab567849b16a2fb9cc583b3
jash.liao <jash.liao@gmail.com>
2017-09-06 21:18:20 +0800

    Create
    https://github.com/jash-git/C-Users-jash-Desktop-C-Disassembler-VS-ifdefine.git

---
+++ Console_ifdefined/Console_ifdefined/Program.cs
@@ -0,0 +1,32 @@
+﻿#define DEBUG
+#define MYTEST 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_ifdefined
+{
+    class Program
+    {
+        static void pause()
+        {
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+        static void Main(string[] args)
+        {
+            #if (DEBUG && !MYTEST)
+				Console.WriteLine("DEBUG is defined");  
+            #elif (!DEBUG && MYTEST)
+				Console.WriteLine("MYTEST is defined");  
+            #elif (DEBUG && MYTEST)
+				Console.WriteLine("DEBUG and MYTEST are defined");
+            #else
+				Console.WriteLine("DEBUG and MYTEST are not defined");  
+            #endif
+            pause();
+        }
+    }
+}
---
+++ Readme.txt
@@ -0,0 +1,5 @@
+﻿C#反組譯TOOL測試 VS ifdefine效果
+	-使用ILSpy_Master_2.4.0.1963_Binaries可以反組譯C#的EXE或DLL
+	-測試如果用#define #if 條件編譯方法 是否可以隱藏部分程式碼
+	
+結論:由結果圖得知 #define #if 是會讓無用程式碼沒有包入EXE中
